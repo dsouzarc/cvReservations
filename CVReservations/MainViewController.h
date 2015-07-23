b2eff7a01fc0ff293bdbbf20e0929e7c7d66b540
Ryan D'souza <dsouzarc@gmail.com>
2015-07-23 15:06:46 -0400

    Settings icon

---
+++ CVReservations/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  CVReservations
+//
+//  Created by Ryan D'souza on 7/23/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainViewController : ViewController
+
+@end
