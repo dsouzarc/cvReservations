92f93b3d41db09694209db3041217a7a2215b958
Ryan D'souza <dsouzarc@gmail.com>
2015-07-23 19:23:57 -0400

    Four activity view controller has four appropriately sized views, began working on Open view

---
+++ CVReservations/FourActivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  FourActivityViewController.h
+//  CVReservations
+//
+//  Created by Ryan D'souza on 7/23/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FourActivityViewController : ViewController
+
+@end
---
+++ CVReservations/FourActivityViewController.m
@@ -0,0 +1,27 @@
+//
+//  FourActivityViewController.m
+//  CVReservations
+//
+//  Created by Ryan D'souza on 7/23/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "FourActivityViewController.h"
+
+@interface FourActivityViewController ()
+
+@property (strong, nonatomic) IBOutlet UIView *firstActivityView;
+@property (strong, nonatomic) IBOutlet UIView *secondActivityView;
+@property (strong, nonatomic) IBOutlet UIView *thirdActivityView;
+@property (strong, nonatomic) IBOutlet UIView *fourthActivityView;
+
+@end
+
+@implementation FourActivityViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
